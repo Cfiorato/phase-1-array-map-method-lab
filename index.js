f676b98f5dd663613ac1ae840826b82863398555
Cameron Fiorato <cameronclayfiorato@gmail.com>
2021-11-27 10:56:36 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -11,6 +11,55 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+//splits the strings by space
+// const splitFn = string => string.split(" ")
+// //Caps first letter in each word
+// function tCaseFn(word){
+//   return word.charAt(0).toUpperCase() + word.slice(1)
+// }
+// //joins array into one string
+// function joinFn(array){
+//   return array.join(" ")
+// }
+// let test = ["toot", "fart", "gas", "flatulence"]
+
+//works doesn't trigger learn though?
+// let tCasedTitles = []
+// function titleCased(tutorials){
+//   let splitStr = tutorials.map(splitFn)
+//   splitStr.map(function(array){
+//     let toAdd = array.map(tCaseFn)
+//     tCasedTitles.push(toAdd)
+//   })
+//   let titleCasedTitles = tCasedTitles.map(joinFn)
+//   return titleCasedTitles
+//   }
+
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(function(title){
+    return title.split(" ").map(function(word){
+      return word.charAt(0).toUpperCase() + word.slice(1)
+    }).join(" ")
+  })
+  // let tCasedTitles = []
+  // let splitStr = tutorials.map(function(string){
+  //   return string.split(" ")
+  // })
+  // splitStr.map(function(array){
+  //   let toAdd = array.map(function(word){
+  //     return word.charAt(0).toUpperCase() + word.slice(1)  
+  //   })
+  //   return tCasedTitles.push(toAdd)
+  // })
+  // let titleCasedTitles = tCasedTitles.map(function(list){
+  //   return list.join(" ")
+  // })
+  // return titleCasedTitles
+
+  }
+
+// const newTitles = titleCased(tutorials);
+
+console.log(titleCased());
+
+// console.log(titleCased(tutorials))
